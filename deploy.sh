d678c529ea4ede48a9ef433b4d9623aaf881082e
공상헌 <boipqiod@naver.com>
2023-10-06 15:33:14 +0900

    TEST. 배포 테스트 용 삭제

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#deploy.sh
+
+# 설정
+FOLDER_NAME=".next"
+BRANCH_NAME="deploy"
+
+# 현재 브랜치의 변경 사항 임시 저장
+git stash
+
+# 브랜치가 이미 존재하는지 확인
+git rev-parse --verify $BRANCH_NAME
+
+# 브랜치가 없으면 생성
+if [ $? -ne 0 ]; then
+  git checkout -b $BRANCH_NAME
+else
+  git checkout $BRANCH_NAME
+fi
+
+# 임시 저장한 변경 사항 적용
+git stash apply
+
+# 특정 폴더만 스테이징
+git add $FOLDER_NAME/
+
+# 커밋
+git commit -m "Deploy commit"
+
+# 푸시
+git push -u origin $BRANCH_NAME
+
+# 원래 브랜치로 돌아감 (예: main 브랜치)
+git checkout -
+
+# 임시 저장한 변경 사항을 다시 적용
+git stash apply
